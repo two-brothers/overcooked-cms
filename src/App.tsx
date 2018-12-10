bdc9038cfe958d2a2c5ecdcf7d3f4d9a51ae3191
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-10 13:00:29 +1100

    Connect the redux store to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div/>
-    );
-  }
+    public render() {
+        return (
+            <Provider store={store}>
+                <div/>
+            </Provider>
+        );
+    }
 }
 
 export default App;

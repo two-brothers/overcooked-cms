741303e8b4c1ff4a385859a6352c066d6b4cdfce
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-13 19:29:10 +1100

    Create Recipe reducer to add multiple recipes and delete a recipe

--- src/reducers/recipe/action.types.ts
+++ src/reducers/recipe/action.types.ts
@@ -2,14 +2,30 @@ import { Action as BaseAction } from 'redux';
 import { IRecipe } from '../../server/interfaces';
 
 /**
+ * Add new recipes to the state
+ */
+export type AddItems = BaseAction<ActionNames.ADD_ITEMS> & {
+    items: IRecipe[];
+}
+
+/**
  * Add a new recipe to the state
  */
 export type AddItem = BaseAction<ActionNames.ADD_ITEM> & {
     item: IRecipe;
 }
 
-export type Action = AddItem;
+/**
+ * Remove a recipe from the state
+ */
+export type RemoveItem = BaseAction<ActionNames.REMOVE_ITEM> & {
+    id: string;
+}
+
+export type Action = AddItems | AddItem | RemoveItem;
 
 export enum ActionNames {
-    ADD_ITEM = 'ADD_RECIPE_ITEM'
+    ADD_ITEM = 'ADD_RECIPE',
+    ADD_ITEMS = 'ADD_RECIPES',
+    REMOVE_ITEM = 'REMOVE_RECIPE'
 }
--- src/reducers/recipe/reducer.ts
+++ src/reducers/recipe/reducer.ts
@@ -5,7 +5,9 @@ export type IState = IRecipe[];
 
 /**
  * Handle the recipe actions as follows
- *  - ADD_ITEM: append a single new recipe item to the state
+ *  - ADD_ITEM: append a single new recipe to the state
+ *  - ADD_ITEMS: append all new recipes to the state
+ *  - REMOVE_ITEM: remove the specified recipe from the state, if it exists
  * @param state the list of recipes before the action is applied
  * @param action the action to apply
  * @returns the list of recipes after the action has been applied
@@ -17,6 +19,13 @@ export default function (state = initialState, action: Action) {
                 ...state,
                 action.item
             ];
+        case ActionNames.ADD_ITEMS:
+            return [
+                ...state,
+                ...action.items
+            ];
+        case ActionNames.REMOVE_ITEM:
+            return state.filter(recipe => recipe.id !== action.id);
         default:
             return state;
     }

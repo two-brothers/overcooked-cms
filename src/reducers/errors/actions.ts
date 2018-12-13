f422423377e80bd9dc1d9c009a2716be676c12da
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-13 16:22:01 +1100

    Use lambda-return simplification in the recordError function

--- src/reducers/errors/actions.ts
+++ src/reducers/errors/actions.ts
@@ -5,12 +5,12 @@ import { ActionNames, AddError } from './action.types';
  * Dispatch the AddError action to add the specified error to the state
  * @param err the error to record
  */
-export const recordError = (err: Error) => (dispatch: Dispatch<AddError>) => {
-    return Promise.resolve({
+export const recordError = (err: Error) => (dispatch: Dispatch<AddError>) =>
+    Promise.resolve({
         error: err.message,
         type: ActionNames.ADD_ERROR
     })
         .then(action => dispatch(action));
-};
+
 
 

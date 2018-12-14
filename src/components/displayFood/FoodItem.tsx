38b6be743a856731a27924423334f1b254db3365
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-14 19:36:46 +1100

    Add edit icon to FoodModal component
    When the edit icon is clicked, a new modal is opened with a
    UpdateFood component

--- src/components/displayFood/FoodItem.tsx
+++ src/components/displayFood/FoodItem.tsx
@@ -1,7 +1,10 @@
 import Fab from '@material-ui/core/Fab';
+import Modal from '@material-ui/core/Modal';
+import Paper from '@material-ui/core/Paper';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import * as React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
@@ -10,12 +13,22 @@ import { IGlobalState } from '../../reducers';
 import { deleteFood } from '../../reducers/food/actions';
 import { IState as IUnit } from '../../reducers/units/reducer';
 import { IFood, IUnitConversion } from '../../server/interfaces';
+import UpdateFood from './UpdateFood';
+
+interface IPassedProps {
+    // the food item to interact with
+    food: IFood;
+}
 
 /**
  * A class to display and interact with a specific food item
  * Note that this class is embedded in a table (which is why it renders a TableRow element)
  */
 class FoodItem extends Component<IProps> {
+    public state: IState = {
+        editing: false
+    };
+
     public render(): JSX.Element {
         return (
             <TableRow>
@@ -26,8 +39,14 @@ class FoodItem extends Component<IProps> {
                         .map(conversion => this.displayConversion(conversion))
                 }</TableCell>
                 <TableCell>
+                    <Fab size={'small'} onClick={this.setEditModalOpen(true)}><EditIcon/></Fab>
                     <Fab size={'small'} onClick={this.remove}><DeleteIcon/></Fab>
                 </TableCell>
+                <Modal open={this.state.editing} onClose={this.setEditModalOpen(false)}>
+                    <Paper>
+                        <UpdateFood item={this.props.food}/>
+                    </Paper>
+                </Modal>
             </TableRow>
         );
     }
@@ -51,12 +70,23 @@ class FoodItem extends Component<IProps> {
     private remove = () => {
         this.props.deleteFood(this.props.food.id);
     };
+
+    /**
+     * Return a function that can be used to either open or close the edit modal
+     * @param open whether the returned function opens (if 'open' is true) or closes the modal
+     */
+    private setEditModalOpen = (open: boolean) => () => {
+        this.setState({editing: open});
+    };
 }
 
-interface IProps {
-    food: IFood;
+type IProps = IPassedProps & {
     units: IUnit;
     deleteFood: (id: string) => Promise<undefined>;
+}
+
+interface IState {
+    editing: boolean;
 }
 
 const mapStateToProps = (state: IGlobalState) => ({

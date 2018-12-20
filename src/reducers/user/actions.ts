c38abeca074f393d333f968983a9970bc96f001b
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-20 16:59:26 +1100

    Integrate Server auth with the redux store
    The Server class provides mechanisms to retrieve the active user
    and to log out. The User reducer has actions to save or clear the
    active user in the store.
    
    This commit adds functions, to be called by the User component,
    that invoke Server authentication operations and then dispatch
    the corresponding User redux action.

---
+++ src/reducers/user/actions.ts
@@ -0,0 +1,32 @@
+import { Dispatch } from 'redux';
+
+import Server from '../../server/server';
+import { AddError } from '../errors/action.types';
+import { recordError } from '../errors/actions';
+import { ActionNames, ClearUser, SetUser } from './action.types';
+
+/**
+ * Retrieve the active user from the server and dispatch SET_USER with the user profile
+ * or CLEAR_USER if there is no active user
+ * Dispatch an error for any unexpected server response
+ */
+export const retrieveUser = () => (dispatch: Dispatch<SetUser | ClearUser | AddError>) =>
+    Server.getActiveUser()
+        .then(profile => profile ?
+            dispatch({
+                profile,
+                type: ActionNames.SET_USER
+            }) :
+            dispatch({type: ActionNames.CLEAR_USER})
+        )
+        .catch(err => recordError(err)(dispatch))
+        .then(() => undefined);
+
+/**
+ * Log the user out of the session and then dispatch CLEAR_USER
+ */
+export const logOut = () => (dispatch: Dispatch<ClearUser | AddError>) =>
+    Server.logOut()
+        .then(() => dispatch({type: ActionNames.CLEAR_USER}))
+        .catch(err => recordError(err)(dispatch))
+        .then(() => undefined);

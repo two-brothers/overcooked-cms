f671b91cee0d436db036b44850e126a27cc30e4c
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2019-01-28 09:08:49 +1100

    Rethrow errors on all reducer actions
    The codebase currently adheres to the following pattern:
    - Every operation is encapsulated in a promise that first completes
      the operation and then updates the redux store
    - If there is an error, the message is added to the redux store and
      the error is suppressed.
    - The components ignore the result of the promise and instead rely
      on changes to the redux store to update their state
    
    The FoodRecord and RecipeRecord components need to know if there was
    an error when retrieving the record (so they can display "Not Found").
    
    This commit changes the pattern so that all errors are also
    passed to the consuming component.
    
    For consistency, it does not suppress any error in any operation.
    The components should handle them explicitly.

--- src/reducers/errors/actions.ts
+++ src/reducers/errors/actions.ts
@@ -2,7 +2,8 @@ import { Dispatch } from 'redux';
 import { ActionNames, AddError } from './action.types';
 
 /**
- * Dispatch the AddError action to add the specified error to the state
+ * Dispatch the AddError action to add the specified error to the state,
+ * and throw the error again so consumers still know about it
  * @param err the error to record
  */
 export const recordError = (err: Error) => (dispatch: Dispatch<AddError>) =>
@@ -10,7 +11,10 @@ export const recordError = (err: Error) => (dispatch: Dispatch<AddError>) =>
         error: err.message,
         type: ActionNames.ADD_ERROR
     })
-        .then(action => dispatch(action));
+        .then(action => dispatch(action))
+        .then(() => {
+            throw err;
+        });
 
 
 
